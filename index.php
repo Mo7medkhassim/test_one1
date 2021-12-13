3d875c33bf75b005c38c014c7e3b31090f754b56
Mohammed Alkhassim <kassimsd350@gmail.com>
2021-12-13 08:06:33 +0400

    test
    test

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+if (!empty($_POST['qr'])) {
+    # code...
+    $qrText = $_POST['qr'];
+    
+    // echo ($qrText);die;
+    // download and required the qrcode library
+    require_once 'phpqrcode/qrlib.php';
+    // to genearate image to use later
+    $path = "img/";
+    // generate uniqid for the file name
+    $file = $path . uniqid() . '.png';
+
+    $text = $qrText;
+    QRcode::png($text, $file, 'L', '10');
+
+    echo ("<img src='" . $file . "'>");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QR code Generate</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <form method="post" action="">
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Enter Text For Generate</label>
+                        <input type="text" name="qr" class="form-control">
+
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</body>
+
+</html>
